4b8c3a6f43f184cee74354a0a0b5d740895e7481
Boris <MTK26@mail.ru>
2024-03-21 20:00:49 +0300

    7. Реализация функции uploading_pet_image в модуле api_petstore, папки images

--- tests/test_petstore.py
+++ tests/test_petstore.py
@@ -2,29 +2,44 @@ import json
 from utils.api_petstore import Petstore_api
 from utils.checking import Checking
 
-lst_status_pets = ['available', 'pending', 'sold']
 id_pet = 123456789
-name = 'King-Kong'
-class Test_petstore():
-    '''Тестирование Зоомагазина'''
+name_category = 'Семейство обезьян'
+name_pet = 'King-Kong'
+url_photo = 'https://img.razrisyika.ru/kart/23/90336-king-kong-3.jpg'
+status = 'available'
+tag_name = 'Обезьяна'
+image = 'images/King-Kong.jpg'
 
-    # def test_find_pet_by_status(self):
-    #     '''Тест на поиск питомца по статусу'''
-    #     for i in lst_status_pets:
-    #         response = Petstore_api.find_pet_by_status(i)
-    #         required_fields = json.loads(response.text)
-    #         Checking.check_all_required_fields(response, required_fields)
-    #         Checking.check_status_code(response, 200)
-    #
-    # def test_add_new_pet(self):
-    #     '''Тест на добавление нового питомца'''
-    #     response = Petstore_api.add_new_pet(id_pet, name)
-    #     required_fields = json.loads(response.text)
-    #     Checking.check_all_required_fields(response, required_fields)
-    #     Checking.check_status_code(response, 200)
+class Test_pets():
+    '''Тестирование питомцев'''
+
+    def test_find_pet_by_status(self):
+        '''Тест на поиск питомца по статусу'''
+        for i in ['available', 'pending', 'sold']:
+            response = Petstore_api.find_pet_by_status(i)
+            required_fields = json.loads(response.text)
+            Checking.check_all_required_fields(response, required_fields)
+            Checking.check_status_code(response, 200)
+
+    def test_add_new_pet(self):
+        '''Тест на добавление нового питомца'''
+        response = Petstore_api.add_new_pet(id_pet, name_category,name_pet,url_photo,status,tag_name )
+        required_fields = json.loads(response.text)
+        Checking.check_all_required_fields(response, required_fields)
+        Checking.check_status_code(response, 200)
 
     def test_search_pet_id(self):
         '''Тест на проверку питомца по ID'''
         response = Petstore_api.search_pet_id(id_pet)
         Checking.check_json_value(response, 'id',id_pet)
         Checking.check_status_code(response, 200)
+
+    def test_uploading_pet_image(self):
+        '''Тест на загрузку картинки питомца по id'''
+        response = Petstore_api.uploading_pet_image(image,'jpeg', id_pet)
+        required_fields = json.loads(response.text)
+        Checking.check_all_required_fields(response, required_fields)
+        Checking.check_status_code(response, 200)
+
+
+
--- utils/api_petstore.py
+++ utils/api_petstore.py
@@ -1,3 +1,5 @@
+import requests
+
 from utils.http_methods import Http_methods
 
 base_url = 'https://petstore.swagger.io/v2'
@@ -16,13 +18,13 @@ class Petstore_api():
         return result_get
 
     @staticmethod
-    def add_new_pet(id_pet, name):
+    def add_new_pet(id_pet, category_name, name_pet,url_photo,status,tag_name):
         '''Добавить нового питомца'''
-        body_json = {"id": id_pet,"category": {"id": 0,"name": "string"},
-                        "name": name,
-                        "photoUrls": ["string"],
-                        "tags": [{"id": 0,"name": "string"}],
-                        "status": "available"}
+        body_json = {"id": id_pet,"category": {"id": id_pet,"name": category_name},
+                        "name": name_pet,
+                        "photoUrls": [url_photo],
+                        "tags": [{"id": id_pet,"name": tag_name}],
+                        "status": status}
         resource_post = '/pet'
         url_post = base_url + resource_post
         result_post = Http_methods.post(url_post, body_json)
@@ -37,3 +39,15 @@ class Petstore_api():
         result_get = Http_methods.get(url_get)
         print(f'Ответ от сервера: {result_get.text}')
         return result_get
+
+    @staticmethod
+    def uploading_pet_image(image, format_image, id_pet):
+        '''Загрузка картинки питомца по id'''
+        data = {'additionalMetadata': format_image}
+        files = {'file': ('image', open(f'{image}', 'rb'),'image/jpeg')}
+        url_post = f'{base_url}/pet/{id_pet}/uploadImage'
+        result_post = requests.post(url_post, data=data, files=files)
+        print(result_post.text)
+        return result_post
+
+
